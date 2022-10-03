39d38d88b658afdc1fe9e83aae093c88aeb546cf
KanuteGX <pimentel2002d@gmail.com>
2022-10-03 15:29:40 -0400

    card qr

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import autoprefixer from 'autoprefixer';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import { resolve } from 'path';
+
+export default defineConfig({
+	publicDir: '../public',
+	plugins: [
+		createHtmlPlugin({
+			minify: true
+		})
+	],
+	root: 'src',
+	build: {
+		outDir: '../dist',
+		emptyOutDir: true,
+		target: 'es2015',
+		rollupOptions: {
+			input: {
+				index: resolve(__dirname, './src/index.html')
+			}
+		}
+	},
+	css: {
+		postcss: {
+			plugins: [autoprefixer()]
+		}
+	}
+});
