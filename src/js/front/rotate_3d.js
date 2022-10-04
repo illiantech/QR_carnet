4a56ac9380abd57909213463f49aa126aedf3978
KanuteGX <pimentel2002d@gmail.com>
2022-10-04 04:06:17 -0400

    pending rotate 3d

---
+++ src/js/front/rotate_3d.js
@@ -0,0 +1,21 @@
+// const $ = (selector) => document.querySelector(selector);
+
+// const wrapper = $('.card-wrapper');
+
+// const card = $('.card');
+
+// // probar con card
+// const { width, height } = wrapper.getBoundingClientRect();
+
+// const halfWidth = width / 2;
+
+// const halfHeight = height / 2;
+
+// wrapper.addEventListener('mousemove', (e) => {
+// 	const { offsetX, offsetY } = e;
+
+// 	const rotateX = ((offsetX - halfHeight) / halfHeight) * 10;
+// 	const rotateY = ((offsetY - halfWidth) / halfWidth) * 10;
+
+// 	card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+// });
