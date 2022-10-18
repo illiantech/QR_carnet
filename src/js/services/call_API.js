9d4819b11c631957e5df9b80a41c027c1c98d4e2
Jesus Medina <medinajesus821@gmail.com>
2022-10-18 10:46:09 -0400

    cambios status

--- src/js/services/call_API.js
+++ src/js/services/call_API.js
@@ -33,62 +33,73 @@ const getData = async () => {
 	try {
 		// Peticion de datos del usuario
 
-		const data = await axios({
-			method: 'GET',
-			url: `http://api-carnet.guarico.gob.ve/client/${pathCI}`
-		}).then((res) => {
-			console.log(res.data);
-			return res.data;
-		});
-
-		const { nombre, cedula_identidad, demonimacion_puesto, deno_cod_secretaria, deno_cod_direccion, fecha_ingreso } = data;
-
-		let nameParse = nombre.toLowerCase().trim().split(/\s+/);
-
-		nameParse = nameParse.filter((item, index) => index < 4);
-
-		name.textContent = nameParse.join(' ');
-
-		ci.textContent = `CI: ${cedula_identidad}`;
-
-		position.textContent = demonimacion_puesto.toLowerCase().trim();
-
-		secrTxt.textContent = deno_cod_secretaria.toLowerCase().trim();
-
-		direcTxt.textContent = deno_cod_direccion.toLowerCase().trim();
-
-		dateEntry.textContent = `Ingresado: ${fecha_ingreso.trim()}`;
-
-		// Peticion del Status del usuario
-
-		const dataStatus = await axios({
+		const { data } = await axios({
 			method: 'GET',
 			url: `http://historial-carnets.guarico.gob.ve/historial?cedula=${pathCI}&Nombre=&fecha=&entregado=false&userViews=0`
-		}).then((res) => {
-			console.log(res.data);
-			return res.data[0][0];
-		});
+		})
 
-		cardStatus.textContent = status[dataStatus.estado];
+		if (data.length > 0) {
+			const [user] = data[0]
+
+			const { Nombre, cedula, Cargo, Dependencia, fecha } = user;
+
+			let nameParse = Nombre.toLowerCase().trim().split(/\s+/);
+
+			nameParse = nameParse.filter((item, index) => index < 4);
+
+			name.textContent = nameParse.join(' ');
+
+			ci.textContent = `CI: ${cedula}`;
+
+			position.textContent = Cargo.toLowerCase().trim();
+
+			secrTxt.textContent = Dependencia.toLowerCase().trim();
+
+			const mapState = (state) => {
+				const states = ['✅ ACTIVO', '❌ INACTIVO', '⚠️ EXTRAVIADO']
+				return states[state] || '❌ INACTIVO'
+			}
+
+			cardStatus.textContent = mapState(user.estado)
+			//direcTxt.textContent = deno_cod_direccion.toLowerCase().trim();
+
+			//dateEntry.textContent = `Ingresado: ${fecha.trim()}`;
+		}
+		// Peticion del Status del usuario
+
+		axios({
+			method: 'GET',
+			url: `http://api-carnet.guarico.gob.ve/photo/${pathCI}`,
+			responseType: 'blob'
+		})
+			.then((res) => {
+				img.src = URL.createObjectURL(res.data);
+			})
+			.catch((err) => {
+				alert(`Error de conexión: \n La 'foto' del usuario no pudo ser encontrada \n ${err} `);
+			});
+
 	} catch (err) {
-		alert(`Error de conexión: \n Los 'datos' del usuario no pueden ser encontrados \n ${err} `);
+		position.textContent = 'Esta persona no se encuentra registrada en nuestra base de datos.'
+		ci.textContent = pathCI
+		name.textContent = '';
+		secrTxt.textContent = '-';
+		direcTxt.textContent = '-';
+		dateEntry.textContent = '-';
+		//alert(`Error de conexión: \n Los 'datos' del usuario no pueden ser encontrados \n ${err} `);
 	}
 };
+/* const dataStatus = await axios({
+	method: 'GET',
+	url: `http://historial-carnets.guarico.gob.ve/historial?cedula=${pathCI}&Nombre=&fecha=&entregado=false&userViews=0`
+}).then((res) => {
+	console.log(res.data);
+	return res.data[0][0];
+});
+ */
+
 
 getData();
 
 // Peticion de foto del usuario
 
-axios({
-	method: 'GET',
-	url: `http://api-carnet.guarico.gob.ve/photo/${pathCI}`,
-	responseType: 'blob'
-})
-	.then((res) => {
-		console.log(res);
-
-		img.src = URL.createObjectURL(res.data);
-	})
-	.catch((err) => {
-		alert(`Error de conexión: \n La 'foto' del usuario no pudo ser encontrada \n ${err} `);
-	});
